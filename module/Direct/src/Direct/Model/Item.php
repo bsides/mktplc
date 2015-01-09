e587517ef93a6443743df52c2bfe1195d7906b51
Rafael Pereira <rafael.pereira@predicta.net>
2015-01-09 19:21:44 -0200

    Determinação incluída no processo de busca

--- module/Direct/src/Direct/Model/Item.php
+++ module/Direct/src/Direct/Model/Item.php
@@ -32,12 +32,15 @@ class Item
         $weekDays[] = 'Ter';
         $weekDays[] = 'Dom';
 
+        $determination = [ 'Padrão', 'Capa', 'Página Direita', 'Sou contra nomes gigantes'];
+
         for($x=0; $x<=rand(3,8); $x++) {
             $deal['id'] = rand(1,999);
             $deal['color'] = $color[rand(0,2)];
             $deal['price'] = 10000.01;
             $deal['column'] = $column[rand(0,1)];
             $deal['format'] = $format[rand(0,2)];
+            $deal['determination'] =$determination[rand(0,3)];
             $deal['name'] = $newspaper[rand(0,2)];
             $deal['weekDay'] = $weekDays[rand(0,2)];
             $deal['image'] = 'http://cdni.wired.co.uk/1920x1280/o_r/Pug.jpg';
