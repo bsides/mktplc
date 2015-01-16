c0ec31429b4381a68a41f51509710c62028ffaea
Rodrigo <rodrigo.souza@predicta.net>
2015-01-16 16:38:11 -0200

    Alterando método de retorno do carrinho de compras

--- module/ShoppingCart/src/ShoppingCart/Controller/IndexController.php
+++ module/ShoppingCart/src/ShoppingCart/Controller/IndexController.php
@@ -1,6 +1,7 @@
 <?php
 namespace ShoppingCart\Controller;
 
+use ArrayObject;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\JsonModel;
 use ShoppingCart\Model;
@@ -10,18 +11,34 @@ class IndexController extends AbstractActionController
     public function indexAction()
     {
         $cart = $this->getServiceLocator()->get('cart.service');
-        return new JsonModel($cart->items);
+        $return = [];
+        if (count($cart->items) > 0) {
+            $array_object = array_map(function($a) { return (new ArrayObject($a))->getArrayCopy(); }, $cart->items);
+            $publishers = array_column(array_column($array_object, 'features'), 'publisher');
+            $publishers[2]['id'] = 123123123;
+            $publishers[2]['description'] = 123123123;
+            foreach($publishers as $publisher) {
+                if (isset($return[$publisher['id']]))
+                    continue;
+
+                $return[$publisher['id']]['id'] = $publisher['id'];
+                $return[$publisher['id']]['description'] = $publisher['description'];
+                $return[$publisher['id']]['items'] = array_filter($array_object, function($item) use ($publisher) { return count(array_diff($item['features']['publisher'], $publisher)) == 0; });
+            }
+            $return = array_filter($return, function($item) { return count($item['items']) > 0; });
+        }
+        return new JsonModel($return);
     }
 
     public function addAction()
     {
-        $request = $this->getRequest();
-        if ($request->isXmlHttpRequest() === FALSE)
-            return new JsonModel([]);
+        if ('0' !== ($hash = (string) $this->params('hash'))) {
+            $service = $this->getServiceLocator()->get('direct.newspaper.item');
+            $params = $this->getRequest()->getQuery()->toArray();
+            $features = array_filter($service->find($params)['data'], function($item) use ($hash) { return $item['hash'] == $hash; });
 
-        $cart = $this->getServiceLocator()->get('cart.service');
-        if ($request->isPost()) {
-            $item = new Model\Item($request->getPost()->toArray());
+            $cart = $this->getServiceLocator()->get('cart.service');
+            $item = new Model\Item(array('hash' => $hash, 'features' => reset($features)));
             $cart->addItem($item);
         }
         return new JsonModel(['message' => 'success']);
@@ -30,12 +47,9 @@ class IndexController extends AbstractActionController
     public function deleteAction()
     {
         $request = $this->getRequest();
-        if ($request->isXmlHttpRequest() === FALSE)
-            return new JsonModel([]);
-
-        if ('0' !== ($id = (string) $this->params('id'))) {
+        if ('0' !== ($hash = (string) $this->params('id'))) {
             $cart = $this->getServiceLocator()->get('cart.service');
-            $cart->removeItem($id);
+            $cart->removeItem($hash);
         }
         return new JsonModel(['message' => 'success']);
     }
@@ -43,9 +57,6 @@ class IndexController extends AbstractActionController
     public function emptyAction()
     {
         $request = $this->getRequest();
-        if ($request->isXmlHttpRequest() === FALSE)
-            return new JsonModel([]);
-
         $cart = $this->getServiceLocator()->get('cart.service');
         $cart->clear();
         return new JsonModel(['message' => 'success']);
